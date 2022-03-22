2b1a0858cc6b4fed520a295af260939577ef759b
Gutemberg S. Mendoza <gutembergmendoza@Gutembergs-MacBook-Pro.local>
2022-03-21 22:27:12 -0500

    Errores provocados por alejandro

---
+++ src/src/main/java/src/SimpleBot.java
@@ -0,0 +1,24 @@
+package src;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class SimpleBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        // TODO
+    }
+
+    @Override
+    public String getBotUsername() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return null;
+    }
+}
+}
